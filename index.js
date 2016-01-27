fa839938e18a131d60242dcc4df079c7b7bf0850
Benjamim Sonntag <benjamimsonntag@gmail.com>
2016-01-27 09:10:55 +0000

    Adds prefix and suffix.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 var isString = require('is-string');
 
 function joinStrings(strings, options) {
-  var separator = '';
+  var delimiter = '';
+  var prefix = '';
+  var suffix = '';
   strings = [].concat(strings);
 
   if(isString(options)) {
-    separator = options;
+    delimiter = options;
   }
-  else if(options && 'separator' in options) {
-    separator = options.separator;
+  else if(options) {
+    delimiter = options.delimiter || '';
+    prefix = options.prefix || '';
+    suffix = options.suffix || '';
   }
 
-  return strings.reduce(concatWithSeparator.bind(null, separator));
+  var joined = strings.reduce(concatWithDelimiter.bind(null, delimiter));
+  return prefix.concat(joined, suffix);
 }
 
-function concatWithSeparator(separator, accumulator, value) {
-  return ''.concat(accumulator, separator, value);
+function concatWithDelimiter(delimiter, accumulator, value) {
+  return ''.concat(accumulator, delimiter, value);
 }
 
 module.exports = joinStrings;
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var joinStrings = require('./index');
 
 var expect = chai.expect;
 
-describe('joinStrings(strings, separator|options)', function() {
+describe('joinStrings(strings, delimiter|options)', function() {
   var strings = ['hello', 'world'];
 
   it('returns a string', function() {
@@ -13,27 +13,39 @@ describe('joinStrings(strings, separator|options)', function() {
 
   describe('with a single string', function() {
     it('returns the string', function() {
-      var result = joinStrings('hello');
-      expect(result).to.be.a('string');
-      expect(result).to.equal('hello');
+      expect(joinStrings('hello')).to.equal('hello');
     });
   });
 
-  describe('with no separator or options', function() {
-    it('returns the concatenated string', function() {
+  describe('with no delimiter or options', function() {
+    it('returns the string joined without delimiter, prefix or suffix', function() {
       expect(joinStrings(strings)).to.equal('helloworld');
     });
   });
 
-  describe('with a separator', function() {
-    it('returns the joined string', function() {
+  describe('with a delimiter', function() {
+    it('returns the string joined with the provided demiliter', function() {
       expect(joinStrings(strings, ',')).to.equal('hello,world');
     });
   });
 
+  describe('with empty options', function() {
+    it('returns the joined string without delimiter, prefix or suffix', function() {
+      expect(joinStrings(strings, {})).to.equal('helloworld');
+    });
+  });
+
   describe('with options', function() {
-    it('returns the joined string', function() {
-      expect(joinStrings(strings, { separator: ',' })).to.equal('hello,world');
+    it('returns the string joined with the provided delimiter', function() {
+      expect(joinStrings(strings, { delimiter: ',' })).to.equal('hello,world');
+    });
+
+    it('returns the string joined and prefixed with the provided prefix', function() {
+      expect(joinStrings(strings, { prefix: '>' })).to.equal('>helloworld');
+    });
+
+    it('returns the string joined and suffixed with the provided suffix', function() {
+      expect(joinStrings(strings, { suffix: '!' })).to.equal('helloworld!');
     });
   });
 });
